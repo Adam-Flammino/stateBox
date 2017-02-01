1baaec7348ef3ab3fd269d2da0b2375ce3b3234f
Adam Flammino <flammino@gmail.com>
2017-02-01 09:30:53 -0500

    cleaned, added DC

---
+++ StateBox.java
@@ -0,0 +1,11 @@
+/**
+ * Populates ComboBox with US state abbreviations 
+ * @param stateBox 
+ */
+    private void StateBox(ComboBox cboStateBox) {
+        cboStateBox.getItems().addAll("N/A", "AK", "AL", "AR", "AZ", "CA", "CO", "CT", 
+                "DC", "DE", "FL", "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY", 
+                "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", 
+                "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI", 
+                "SC", "SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV", "WY");
+    }
